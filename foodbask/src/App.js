c30c8a2c39e82941b87550040bea4e2a18b8d3f7
titosco <titobiloluwatella@gmail.com>
2024-08-18 23:00:01 +0100

    creating nav func

---
+++ foodbask/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navigation from './Navigation';
+
+function App() {
+  return (
+    <div className="App text-gray-600 font-body grid md:grid-cols-3">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ foodbask/src/Navigation.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import React from "react";
+
+const Navigation = () => {
+  return <div>
+    <nav>
+        <div>
+            <h1>
+                <a href="/">Food Bask</a>
+            </h1>
+            <div>
+
+            </div>
+        </div>
+    </nav>
+  </div>;
+};
+
+export default Navigation;
